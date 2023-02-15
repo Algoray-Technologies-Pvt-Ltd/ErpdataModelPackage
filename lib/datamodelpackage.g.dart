621f2051acf3bb0adb424bc89dab4c20b4aa7fde
deeomuhammed <domuhammed5@gmail.com>
2023-02-15 17:15:32 +0530

    hive

--- lib/datamodelpackage.g.dart
+++ lib/datamodelpackage.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'GeneralVoucherDataModel.dart';
+part of 'datamodelpackage.dart';
 
 // **************************************************************************
 // TypeAdapterGenerator
