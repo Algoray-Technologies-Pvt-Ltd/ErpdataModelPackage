cb5d38e8445e430c0bde46d4f264dde6933f9508
deeomuhammed <domuhammed5@gmail.com>
2023-02-17 18:49:48 +0530

    hiv

--- lib/datamodelpackage.dart
+++ lib/datamodelpackage.dart
@@ -1,7 +1,18 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:datamodelpackage/transaction/GeneralVoucherDataModel.dart';
+import 'package:hive_flutter/adapters.dart';
+
 class Deeo {
   String de;
+  var cakeBox;
   Deeo({
     required this.de,
   });
+
+  hiveinit() async {
+    await Hive.initFlutter();
+    Hive.registerAdapter(GeneralVoucherDataModelAdapter());
+
+    this.cakeBox = await Hive.openBox<GeneralVoucherDataModel>('cakeBo');
+  }
 }
